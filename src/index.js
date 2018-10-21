e7d6eefcf588d9316842c148c92209a68015a303
unknown <saherelgendy1990@gmail.com>
2018-10-21 03:23:51 +0200

    removing redundant variable

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/index';
 
