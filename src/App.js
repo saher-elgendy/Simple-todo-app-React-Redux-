81f7626b8bd4cdde2a1a14b32c0b6ec4cd22aebc
unknown <saherelgendy1990@gmail.com>
2018-10-21 03:18:16 +0200

    adding semicolon

--- src/App.js
+++ src/App.js
@@ -115,4 +115,4 @@ const mapDispatchToProps= (dispatch) => {
   
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
