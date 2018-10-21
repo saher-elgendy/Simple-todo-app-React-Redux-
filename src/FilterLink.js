be496f9cfe445e7a9b2c8ff39a6ab5889dbb18b0
unknown <saherelgendy1990@gmail.com>
2018-10-21 03:21:03 +0200

    destructing props object in filter link component

--- src/FilterLink.js
+++ src/FilterLink.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 const FilterLink = (props) => {
+  const {setFilter, filter, child} = props;
 
   return (
     <a  
       href="#" 
-      onClick={() => {props.setFilter(props.filter)}}
-    >{props.child}</a>
+      onClick={() => {setFilter(filter)}}
+    >{child}</a>
   );
 }
 
